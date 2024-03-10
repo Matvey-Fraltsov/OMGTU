863a296c540ae0aa16f3bb9c82843412a7aa4f4b
Matvey-Fraltsov <145004799+Matvey-Fraltsov@users.noreply.github.com>
2024-03-11 01:09:37 +0600

    Update Зельеварение.py

--- Olymp/Зельеварение.py
+++ Olymp/Зельеварение.py
@@ -1,12 +1,9 @@
-#DUST root tooth
-#WATER 1 tear
-
 class Zelie:
     def __init__(self):
         self.data = []
         self.magic = []
 
-    def MIX(self, *args):
+    def MIX(self, args):
         for i in args:
             if type(i) == int:
                 i = self.data[i - 1]
@@ -14,8 +11,7 @@ class Zelie:
         self.data.append(f"MX{(''.join(self.magic)).replace(' ', '')}XM")
         self.magic.clear()
 
-    def WATER(self, *args):
-        magic = []
+    def WATER(self, args):
         for i in args:
             if type(i) == int:
                 i = self.data[i - 1]
@@ -23,8 +19,7 @@ class Zelie:
         self.data.append(f"WT{(''.join(self.magic)).replace(' ', '')}TW")
         self.magic.clear()
 
-    def DUST(self, *args):
-        magic = []
+    def DUST(self, args):
         for i in args:
             if type(i) == int:
                 i = self.data[i - 1]
@@ -32,8 +27,7 @@ class Zelie:
         self.data.append(f"DT{(''.join(self.magic)).replace(' ', '')}TD")
         self.magic.clear()
 
-    def FIRE(self, *args):
-        magic = []
+    def FIRE(self, args):
         for i in args:
             if type(i) == int:
                 i = self.data[i - 1]
@@ -41,12 +35,21 @@ class Zelie:
         self.data.append(f"FR{(''.join(self.magic)).replace(' ', '')}RF")
         self.magic.clear()
 
+for test in range(1, 11):
+    with open(f'Зельеварение/input{test}.txt') as f:
+        inp = [i.split() for i in f.read().splitlines()]
+    with open(f'Зельеварение/output{test}.txt') as f:
+        out = f.read()
 
+    x = Zelie()
+    for i in range(len(inp)):
+        for j in range(len(inp[i])):
+            try:
+                inp[i][j] = int(inp[i][j])
+            except ValueError:
+                continue
+        x.FIRE(inp[i][1:]) if inp[i][0] == "FIRE" else (x.MIX(inp[i][1:]) if inp[i][0] == "MIX" else
+                                                (x.WATER(inp[i][1:]) if inp[i][0] == "WATER" else x.DUST(inp[i][1:])))
 
-x = Zelie()
-x.DUST('XqK', 'DWYMfDBZ', 'ZZdnlqo')
-x.FIRE(1, 'Odg')
-x.WATER(1)
-x.MIX(3, 2, 'xJZYkKXL')
-x.DUST(4, 'ceeJr')
-print(x.data[-1])
+    print(x.data[-1] == out)
+
