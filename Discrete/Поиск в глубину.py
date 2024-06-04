2295d44bc6f2adda9d2b785ff524bb9de828dd26
Matvey-Fraltsov <145004799+Matvey-Fraltsov@users.noreply.github.com>
2024-06-04 09:26:51 +0600

    Create Поиск в глубину.py

---
+++ Discrete/Поиск в глубину.py
@@ -0,0 +1,23 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=' ')
+
+    for i, node in enumerate(graph[start]):
+        if node and i not in visited:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1, 0],
+    [1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0],
+]
+
+start_node = 0
+print("Обход в глубину из вершины:", start_node)
+dfs(graph, start_node)
